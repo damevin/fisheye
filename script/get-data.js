a3302e3700cba8545dd7c61757b7ec4c82a8589d
DAMEVIN <raphael.damevin@gmail.com>
2021-04-16 11:24:29 +0200

    Implement display data, try to display photographer infos

--- script/get-data.js
+++ script/get-data.js
@@ -1,15 +1,20 @@
 // GET 
-const getPhotographers = async function () {
+const getData = async function () {
   let response = await fetch("../data/photographers.json", {mode: 'no-cors'})
   if (response.ok) {
     let data = await response.json()
-    console.log(data)
+    return data;
   } else {
     console.error(response.status)
   }
 }
 
-getPhotographers()
+const displayData = async function() {
+  const data = await getData();
+  const element = document.querySelector('.photographer');
+  for (const photographer of data.photographers) {
+    element.innerHTML = `${photographer.name} ${photographer.id} ${photographer.city}`
+  }
+}
 
-
- 
+displayData()
