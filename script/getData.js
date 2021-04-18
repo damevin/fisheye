a5bf46455bd69ed7d4287c8a8073d6dbf8403b69
DAMEVIN <raphael.damevin@gmail.com>
2021-04-18 20:09:33 +0200

    Improve getData

--- script/getData.js
+++ script/getData.js
@@ -5,19 +5,18 @@ const getData = async () => await fetch("../data/photographers.json", {mode: 'no
 
 const displayData = async () => {
   const { media, photographers } = await getData();
-  console.log(photographers)
   const element = document.querySelector('.photographer');  
 
   photographers.forEach( photographer => element.innerHTML += `
-  <article>
+  <div>
   <img class="photographer__img" src="./assets/photographers/${photographer.portrait}">
   <h2 class="photographer__name">${photographer.name}</h2>
   <p class="photographer__localization">${photographer.city}, ${photographer.country}</p>
   <p class="photographer__tagline">${photographer.tagline}</p>
   <ul>
-  <li></li>
+  <li>${photographer.tags}</li>
   </ul>
-  </article>`
+  </div>`
   )}
 
 displayData()
