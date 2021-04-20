b529de36c94e4d2a3f7203bb39ee0e739accac5b
DAMEVIN <raphael.damevin@gmail.com>
2021-04-20 20:14:17 +0200

    [WIP] Create for loop for get tags

--- script/getData.js
+++ script/getData.js
@@ -6,29 +6,24 @@ const getData = async () => await fetch("../data/photographers.json", {mode: 'no
 const displayData = async () => {
   const { media, photographers } = await getData();
   const element = document.querySelector('.photographer');
- /*  const tagsList = document.querySelector('.photographer__tags');
-  const node = document.createElement("LI");
-  node.appendChild(tagsList); */
 
-  photographers.forEach( photographer => element.innerHTML += `
-  <div class="photographer">
-  <img class="photographer__img" src="./assets/photographers/${photographer.portrait}">
-  <h2 class="photographer__name">${photographer.name}</h2>
-  <p class="photographer__localization">${photographer.city}, ${photographer.country}</p>
-  <p class="photographer__tagline">${photographer.tagline}</p>
-  <ul class="photographer__tags">
-  </ul>
-  </div>`
-  )
-  
-  photographers.forEach( tags => createTags())
-}
+  photographers.forEach( photographer => {
+    const tags = photographer.tags
+    element.innerHTML += `
+      <div class="photographer">
+      <img class="photographer__img" src="./assets/photographers/${photographer.portrait}">
+      <h2 class="photographer__name">${photographer.name}</h2>
+      <p class="photographer__localization">${photographer.city}, ${photographer.country}</p>
+      <p class="photographer__tagline">${photographer.tagline}</p>
+      <ul class="photographer__tags">
+      </ul>
+      </div>`;
+    for (var i = 0; i < tags.length; i++) {
+      const tagList = document.querySelector(".photographer__tags");
+      const li = document.createElement('li');
+      li.textContent = photographer.tags[i]
+      tagList.appendChild(li);
+  }
+})}
 
 displayData()
-
-function createTags(){
-  const tagList = document.querySelector(".photographer__tags");
-  const li = document.createElement('li');
-  li.textContent = photographers.tags;
-  tagList.appendChild(li);
-}
