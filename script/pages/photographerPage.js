4246d9df2ad0e126d4d6d161cbd363f10176847a
DAMEVIN <raphael.damevin@gmail.com>
2021-04-29 15:11:01 +0200

    display data in photographer page

--- script/pages/photographerPage.js
+++ script/pages/photographerPage.js
@@ -1,15 +1,15 @@
 async function displayPhotographerData() {
     const { media, photographers } = await getData()
-    /* const Photographers = photographers.map(photographer => new Photographer(photographer)) */
-    const urlParams = new URLSearchParams(window.location.search);
-    const photographerID = urlParams.get('id');
-    const index = parseInt(photographerID);
-    if (isNaN(index)) {
-        console.log("******Cassé*****")
-    } else {    
-        let getThisPhotographer = photographers[index]
-        console.log(getThisPhotographer)
-    }  
+    const id = window.location.search.split('id=')[1];
+    const photographer = !id ? photographers : photographers.filter(photographer => photographer.id == id);
+    const $photographerHeader = document.querySelector('.photographer-page')
+    $photographerHeader.innerHTML += `
+        <h1>${photographer[0].name}</h1>
+        <p>${photographer[0].city}, ${photographer[0].country} </p>
+        <img src="../assets/photographers/${photographer[0].portrait}">
+        `
+    console.log(photographer[0])
+ 
 }
 
 displayPhotographerData()
