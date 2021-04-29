afd8f8433804c3aef5aef10a1c0b009c92a03378
DAMEVIN <raphael.damevin@gmail.com>
2021-04-29 15:40:37 +0200

    Refactor variable

--- script/pages/photographerPage.js
+++ script/pages/photographerPage.js
@@ -1,13 +1,13 @@
 async function displayPhotographerData() {
     const { media, photographers } = await getData()
     const id = window.location.search.split('id=')[1];
-    const photographer = !id ? photographers : photographers.filter(photographer => photographer.id == id);
+    const photographer =  photographers.find(photographer => photographer.id == id);
     const $photographerHeader = document.querySelector('.photographer-page')
     $photographerHeader.innerHTML += `
-        <h1>${photographer[0].name}</h1>
-        <p>${photographer[0].city}, ${photographer[0].country} </p>
-        <img src="../assets/photographers/${photographer[0].portrait}">
-        <p >${photographers[0].tags.map(tag => `<a href="../index.html">#${tag}</a>`).join(" ")}</p>
+        <h1>${photographer.name}</h1>
+        <p>${photographer.city}, ${photographer.country} </p>
+        <img src="../assets/photographers/${photographer.portrait}">
+        <p >${photographer.tags.map(tag => `<a href="../index.html">#${tag}</a>`).join(" ")}</p>
         `
     console.log(photographer[0])
  
