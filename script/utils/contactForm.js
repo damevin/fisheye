5f66e928428c4da1c3cc25b8ad0d883fe2ca3a4f
DAMEVIN <raphael.damevin@gmail.com>
2021-05-11 14:38:10 +0200

    Change variables attributes

--- script/utils/contactForm.js
+++ script/utils/contactForm.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
