ab7a8ec6aaedd275d28cb8b2323d68631ab2b1bd
DAMEVIN <raphael.damevin@gmail.com>
2021-05-11 13:21:26 +0200

    WIP - Media constructor

--- script/constructors/Media.js
+++ script/constructors/Media.js
@@ -7,12 +7,24 @@ class Media {
   this.likes = data.likes;
   this.date = data.date;
   this.price = data.price;
-  
  }
  
 }
 
 class Photography extends Media {
+ createHTML() {
+  let elementPhotography = document.createElement('img');
+  elementPhotography.setAttribute('src', this.image);
+  elementPhotography.setAttribute('alt', this.alt);
+  elementPhotography.setAttribute('role', 'button');
+  elementPhotography.className = 'photographer-page__gallery__photography';
+  return eltImage;
+}
+
+get createElement() {
+ return `
+ <img class="photographer-page__gallery__photography" src='../assets/medias/${photographer.name}/${image.image}'>`
+}
 
 }
 
