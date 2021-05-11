5d82c03f920d8fcbb8216000083ac9780178e0c9
DAMEVIN <raphael.damevin@gmail.com>
2021-05-11 15:18:27 +0200

    Improve documentation

--- script/utils/contactForm.js
+++ script/utils/contactForm.js
@@ -1,42 +1,48 @@
+/**
+ * DOM elements
+ */
 const modal = document.getElementById("contact__modal");
 const btn = document.getElementById("photographer-page__contact__button");
 const span = document.getElementsByClassName("close")[0];
-
-//Submit button
-const submitButton = document.getElementById('form-submit-button')
-
-// DOM ELEMENTS 
 const firstNameInput = document.getElementById('firstname')
 const lastNameInput = document.getElementById('lastname')
 const emailInput = document.getElementById('email')
 const messageInput = document.getElementById('message')
+const submitButton = document.getElementById('form-submit-button')
 
-console.log(emailInput)
-
-// When the user clicks the button, open the modal 
+/**
+ * Display the modal when the user click on the button
+ * @return {string}
+ */
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+/**
+ * Undisplay the modal when the user click on the span
+ * @return {string}
+ */
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+/**
+ * Undisplay the modal if the user click anywhere outside
+ */
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-console.log(submitButton)
+/**
+ * Send form with inputs values in the console
+ */
 if (submitButton) {
  submitButton.addEventListener("click", function(event) {
   event.preventDefault()
   console.log(`L'utilisateur ${firstNameInput.value} ${lastNameInput.value} avec l'adresse mail suivante ${emailInput.value} vous adresse le message suivant : ${messageInput.value}`)
   modal.style.display = "none";
  })
-
 }
 
