85eb1662eb2b6a94ab6b327f4c56c054230952d8
DAMEVIN <raphael.damevin@gmail.com>
2021-05-12 08:37:22 +0200

    Update Media constructor

--- script/constructors/Media.js
+++ script/constructors/Media.js
@@ -1,33 +1,45 @@
 class Media {
+  /**
+   * @constructor
+   * @param {number} data.id
+   * @param {string} data.photographerId
+   * @param {string} data.filename
+   * @param {array | string} data.tags
+   * @param {number} data.likes
+   * @param {number} data.price
+   * @param {string} data.altText
+   */
  constructor(data){
-  this.id = id;
-  this.image = data.image;
-  this.video = data.video;
-  this.imgAlt = data.imgAlt;
-  this.likes = data.likes;
-  this.date = data.date;
-  this.price = data.price;
+  this.id = data.id
+  this.photographerId = data.photographerId
+  this.filename = data.filename
+  this.tags = data.tags
+  this.price = data.price
+  this.altText = data.altText
  }
- 
 }
 
 class Photography extends Media {
- createHTML() {
-  let elementPhotography = document.createElement('img');
-  elementPhotography.setAttribute('src', this.image);
-  elementPhotography.setAttribute('alt', this.alt);
-  elementPhotography.setAttribute('role', 'button');
-  elementPhotography.className = 'photographer-page__gallery__photography';
-  return eltImage;
-}
+  /**
+   * Récupère le bon chemin pour la source de la photographie
+   * @returns {string}
+   */
+  createSource() {
+    return `
+    src='../assets/medias/${this._name}/${this._image}'`
+  }
 
-get createElement() {
- return `
- <img class="photographer-page__gallery__photography" src='../assets/medias/${photographer.name}/${image.image}'>`
-}
-
-}
-
-class Video extends Media {
+  /**
+   * Créer l'élément HTML de la photographie
+   * @returns {HTMLElement}
+   */
+  createHTML() {
+    let elementPhotography = document.createElement('img')
+    elementPhotography.setAttribute('alt', this.alt)
+    elementPhotography.setAttribute('role', 'button')
+    elementPhotography.setAttribute('src', this.createSource)
+    elementPhotography.className = 'photographer-page__gallery__photography'
+    return elementPhotography;
+  }
 
 }
