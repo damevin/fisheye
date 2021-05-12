00d0a8a50eb8abdaa77db3fcd06469829bb0e0d8
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-05-12 09:55:50 +0200

    Add MediaFactory example

---
+++ script/constructors/Media.example.js
@@ -0,0 +1,45 @@
+class MediaFactory {
+    constructor(data, type) {
+
+        console.log("====")
+        console.log(type)
+        console.log("====")
+        
+        if (type === 'photography') {
+            return new Photography(data)
+        } else if (type === 'video') {
+            return new Video(data)
+        } else {
+            throw 'Unknown Media Type'
+        }
+    }
+}
+
+class Photography {
+    constructor(data) {
+        this._imgSrc = data.src
+        this._imgAlt = data.alt
+    }
+
+    createHtml() {
+        return `<img src="${this._imgSrc}" alt="${this._imgAlt}" />`
+    }
+}
+
+class Video {
+    constructor(data) {
+        this._src = data.src
+        this._title = data.title
+    }
+
+    createHtml() {
+        return `
+            <video controls>
+                <source src="${this._src}" />
+            </video>
+        `
+    }
+}
+
+const PhotographyTest = new MediaFactory({ src: "sdsdsds", alt: "sdsddsd" }, 'photography')
+console.log(PhotographyTest.createHtml())
