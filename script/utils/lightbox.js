491a74c3ef2ccf0ef963abdc702bee269f4d7a19
DAMEVIN <raphael.damevin@gmail.com>
2021-06-03 13:40:26 +0200

    Add alt attribute based on img src

--- script/utils/lightbox.js
+++ script/utils/lightbox.js
@@ -4,124 +4,131 @@
  * @property {string} src image affichée
  */
 class Lightbox {
- static init () {
-  const links = Array.from(document.querySelectorAll('.photographer-page__gallery__media','img', 'video'))
-  const gallery = links.map(link => link.getAttribute('src'))
-  links.forEach(link => link.addEventListener('click', e => {
-     e.preventDefault()
-     new Lightbox(e.currentTarget.getAttribute("src"), gallery)
-   }))
-   
- }
+	static init() {
+		const links = Array.from(
+			document.querySelectorAll(".photographer-page__gallery__media", "img", "video")
+		);
+		const gallery = links.map((link) => link.getAttribute("src"));
+		links.forEach((link) =>
+			link.addEventListener("click", (e) => {
+				e.preventDefault();
+				new Lightbox(e.currentTarget.getAttribute("src"), gallery);
+			})
+		);
+	}
 
- /**
-  * 
-  * @param {string} url Media URL
-  * @param {string[]} gallery Tableau d'url des médias
-  */
- constructor (url, gallery) {
-  this.element = this.buildDOM(url)
-  this.gallery = gallery
-  this.loadMedia(url)
-  this.onKeyUp = this.onKeyUp.bind(this)
-  document.body.appendChild(this.element)
-  document.addEventListener('keyup', this.onKeyUp)
- }
+	/**
+	 *
+	 * @param {string} url Media URL
+	 * @param {string[]} gallery Tableau d'url des médias
+	 */
+	constructor(url, gallery, alt) {
+		this.element = this.buildDOM(url, alt);
+		this.gallery = gallery;
+		this.loadMedia(url);
+		this.onKeyUp = this.onKeyUp.bind(this);
+		document.body.appendChild(this.element);
+		document.addEventListener("keyup", this.onKeyUp);
+	}
 
- /**
-  * @param {*string} url Media URL
-  */
- loadMedia(url){
-  this.url = null
-  const image = new Image()
-  const container = this.element.querySelector('.lightbox__container')
-  const loader = document.createElement("div")
-  loader.classList.add('lightbox__loader')
-  container.innerHTML = ''
-  container.appendChild(loader)
-  image.onload = () => {
-   container.removeChild(loader)
-   container.appendChild(image)
-   this.url = url
-  }
-  image.src = url
+	/**
+	 * @param {*string} url Media URL
+	 */
+	loadMedia(url, alt) {
+		this.url = null;
+		this.alt = null;
+		const image = new Image();
+		const container = this.element.querySelector(".lightbox__container");
+		const loader = document.createElement("div");
+		loader.classList.add("lightbox__loader");
+		container.innerHTML = "";
+		container.appendChild(loader);
+		image.onload = () => {
+			container.removeChild(loader);
+			container.appendChild(image);
+			this.url = url;
+			this.alt = alt;
+		};
+		image.alt = this.getFormatedTitle(url);
+		image.src = url;
+	}
 
- }
+	getFormatedTitle(path) {
+		const EXPR_IMG_TITLE = /^.+\/([^/]+)\.[a-z]{3,4}$/i;
+		const string = path.match(EXPR_IMG_TITLE)[1] || path;
+		const formatedTitle = string.replace("_", " ");
+		return formatedTitle;
+	}
 
- /**
-  * @param {KeyboardEvent} e
-  */
- onKeyUp(e) {
-  if (e.key === 'Escape') {
-   this.close(e)
-  } else if (e.key === 'ArrowLeft') {
-   this.next(e)
-  } else if (e.key === 'ArrowRight') {
-   this.previous(e)
-  }
- }
+	/**
+	 * @param {KeyboardEvent} e
+	 */
+	onKeyUp(e) {
+		if (e.key === "Escape") {
+			this.close(e);
+		} else if (e.key === "ArrowLeft") {
+			this.next(e);
+		} else if (e.key === "ArrowRight") {
+			this.previous(e);
+		}
+	}
 
-/**
- * Ferme la modal
- * @param {MouseEvent | KeyboardEvent} e 
- */
- close(e) {
-  e.preventDefault()
-  this.element.classList.add('fadeOut')
-  window.setTimeout(() => {
-   this.element.parentElement.removeChild(this.element)
-  }, 500)
-  document.removeEventListener('keyup', this.onKeyUp)
- }
+	/**
+	 * Ferme la modal
+	 * @param {MouseEvent | KeyboardEvent} e
+	 */
+	close(e) {
+		e.preventDefault();
+		this.element.classList.add("fadeOut");
+		window.setTimeout(() => {
+			this.element.parentElement.removeChild(this.element);
+		}, 500);
+		document.removeEventListener("keyup", this.onKeyUp);
+	}
 
+	/**
+	 * Navigue vers l'image suivante
+	 * @param {MouseEvent | KeyboardEvent} e
+	 */
+	next(e) {
+		e.preventDefault();
+		let i = this.gallery.findIndex((image) => image === this.url);
+		if (i === this.gallery.length - 1) {
+			i = -1;
+		}
+		this.loadMedia(this.gallery[i + 1]);
+	}
 
-/**
- * Navigue vers l'image suivante
- * @param {MouseEvent | KeyboardEvent} e 
- */
- next(e){
-  e.preventDefault()
-  let i = this.gallery.findIndex(image => image === this.url)
-  if (i === this.gallery.length - 1) {
-   i = -1
-  }
-  this.loadMedia(this.gallery[i + 1])
- }
+	/**
+	 * Navigue vers l'image précédente
+	 * @param {MouseEvent | KeyboardEvent} e
+	 */
+	previous(e) {
+		e.preventDefault();
+		let i = this.gallery.findIndex((image) => image === this.url);
+		if (i === 0) {
+			i = this.gallery.length;
+		}
+		this.loadMedia(this.gallery[i - 1]);
+	}
 
- /**
- * Navigue vers l'image précédente
- * @param {MouseEvent | KeyboardEvent} e 
- */
- previous(e) {
-  e.preventDefault()
-  let i = this.gallery.findIndex(image => image === this.url)
-  if (i === 0) {
-   i = this.gallery.length
-  }
-  this.loadMedia(this.gallery[i - 1])
-
- }
-
- /**
-  * 
-  * @param {string} url Media URL
-  * @return {HTMLElement}
-  */
- buildDOM(url) {
-  const dom = document.createElement('div')
-  dom.classList.add('lightbox')
-  dom.innerHTML = `
-  <button class="lightbox__close">Fermer</button>
-  <button class="lightbox__next">Suivant</button>
-  <button class="lightbox__previous">Précédent</button>
-  <div class="lightbox__container">
-  </div>`
-  dom.querySelector('.lightbox__close').addEventListener('click', 
-  this.close.bind(this))
-  dom.querySelector('.lightbox__next').addEventListener('click', 
-  this.next.bind(this))
-  dom.querySelector('.lightbox__previous').addEventListener('click', 
-  this.previous.bind(this))
-  return dom
- }
+	/**
+	 *
+	 * @return {HTMLElement}
+	 */
+	buildDOM() {
+		const dom = document.createElement("div");
+		dom.classList.add("lightbox");
+		dom.innerHTML = `
+    <button class="lightbox__close" aria-label="Close dialog">Fermer</button>
+    <button class="lightbox__next" aria-label="Next image">Suivant</button>
+    <button class="lightbox__previous" aria-label="Previous image">Précédent</button>
+    <div class="lightbox__container" role="dialog" aria-label="image closeup view">
+    <p class="lightbox__container__img-title"></p>
+    </div>`;
+		dom.querySelector(".lightbox__close").addEventListener("click", this.close.bind(this));
+		dom.querySelector(".lightbox__next").addEventListener("click", this.next.bind(this));
+		dom.querySelector(".lightbox__previous").addEventListener("click", this.previous.bind(this));
+		return dom;
+	}
 }
