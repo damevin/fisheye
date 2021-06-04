bb8801dcfe6fbba98fbc059dd88fcb3cf5207dc6
DAMEVIN <raphael.damevin@gmail.com>
2021-06-04 11:24:50 +0200

    WIP - Likes, and documentation

--- script/pages/photographerPage.js
+++ script/pages/photographerPage.js
@@ -1,6 +1,11 @@
 const $elementGallery = document.querySelector(".photographer-page__gallery");
 
-
+/**
+ * Filter media gallery with select option params
+ * @param {Array} mediaGallery
+ * @param {String} option
+ * @returns {Array} - return a sorted media array
+ */
 const filterByOption = (mediaGallery, option) => {
 	switch (option) {
 		case "popularity":
@@ -18,34 +23,43 @@ const filterByOption = (mediaGallery, option) => {
 				return b.likes - a.likes;
 			});
 	}
-}
+};
 
+/**
+ * @async Display photographer data, based on his ID
+ *	@constant {Array | Objects} media & photographers
+ * @constant params - Get params in URL
+ * @constant identifier - Get id in params
+ * @constant selectedPhotographerData - return
+ * @constant $photographerHeader {HTMLElement} - Get html for place photographer header
+ * @constant mediaGallery - Return array of medias, based on photographer identifier
+ * @function updateMediaGallery
+	* @event listen when user change select state
+ */
 async function displayPhotographerData() {
 	const { media, photographers } = await getData();
 	const params = new URLSearchParams(document.location.search.substring(1));
-	let identifier = params.get("id");
+	const identifier = params.get("id");
 	const selectedPhotographerData = photographers.find(
 		(photographer) => photographer.id == identifier
 	);
-	const $photographerHeader = document.querySelector(
-		".photographer-page__header-section"
-	);
-	$photographerHeader.innerHTML += new Photographer(
-		selectedPhotographerData
-	).userHeader;
-	const mediaGallery = media.filter(
-		(media) => media.photographerId == identifier
-	);
+	const $photographerHeader = document.querySelector(".photographer-page__header-section");
+	$photographerHeader.innerHTML += new Photographer(selectedPhotographerData).userHeader;
+	const mediaGallery = media.filter((media) => media.photographerId == identifier);
+
 	updateMediaGallery(mediaGallery);
+
 	document.addEventListener("change", function (event) {
 		$elementGallery.innerHTML = "";
-		
-		const option = filterByOption(mediaGallery, event.target.value)
-		updateMediaGallery(option)
-
+		const option = filterByOption(mediaGallery, event.target.value);
+		updateMediaGallery(option);
 	});
 }
 
+/**
+ * Update media gallery
+ * @param {Array} gallery
+ */
 function updateMediaGallery(gallery) {
 	gallery.forEach((media) => {
 		let medias = new MediaFactory(media);
@@ -53,11 +67,26 @@ function updateMediaGallery(gallery) {
 	});
 }
 
-function getUrlParams() {}
+function incrementLikes() {
+	console.log("=====");
+	console.log("next step");
+	console.log("=====");
+}
 
-const main = async () => {
+/**
+	* Function for initialized page
+	*/
+const init = async () => {
 	await displayPhotographerData();
 	Lightbox.init();
+
+	const $likesButtons = document.querySelectorAll(
+		".photographer-page__gallery__media__footer__like-section-button"
+	);
+
+	$likesButtons.forEach((likeButton) => {
+		likeButton.addEventListener("click", incrementLikes);
+	});
 };
 
-main();
+init();
