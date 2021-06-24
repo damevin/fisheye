029437ef45eba2aadbc4f331f8d92f862d28bb17
DAMEVIN <raphael.damevin@gmail.com>
2021-06-24 14:10:35 +0200

    Remove asynchronious because its useless broo

--- script/utils/fetchAPI.js
+++ script/utils/fetchAPI.js
@@ -1,3 +1,3 @@
-const getData = async () => await fetch("../data/photographers.json", {mode: 'no-cors'})
+const getData = () => fetch("../data/photographers.json", {mode: 'no-cors'})
   .then(res => res.json())
   .catch(err => console.log("An error occurs when fetching photographers", err))
